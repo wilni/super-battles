efba1a9e6897f5c5a332416500bf5b19d78c330b
Will Tavarez <will@Wills-MBP.localdomain>
2022-07-15 12:45:33 -0400

    selection for opponents added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import axios from 'axios';
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -14,18 +13,69 @@ const dcHeros = heroList.filter(hero => hero.publisher === "DC Comics");
 class App extends React.Component {
 
   state = {
+    choosingOpponent: false,
     universe: marvelHeros,
     heroName: null,
-    heroData: {}
+    heroData: {
+      id: '1',
+      name: 'A-Bomb',
+      powerstats: {
+        intelligence: '38',
+        strength: '100',
+        speed: '17',
+        durability: '80',
+        power: '24',
+        combat: '64'
+      },
+      image: 'https://www.superherodb.com/pictures2/portraits/10/100/10060.jpg'
+    },
+    opponentName: "super pup",
+    opponentData: {
+      id: '1',
+      name: 'super pup',
+      powerstats: {
+        intelligence: '100',
+        strength: '100',
+        speed: '100',
+        durability: '100',
+        power: '100',
+        combat: '100'
+      },
+      image: 'https://images.unsplash.com/photo-1532202802379-df93d543bac3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+    }
   }
 
   handleChange = (e) => {
-    this.setState({
-      heroName: e.target.value
-    })
+    if(this.state.choosingOpponent){
+      this.setState({
+        opponentName: e.target.value
+      })
+    }else{
+      this.setState({
+        heroName: e.target.value
+      })
+    }
+
   };
 
   handleClick = (e) => {
+    e.preventDefault();
+    console.log('were in the button');
+    let opponentUniverse; 
+    if(this.state.universe === marvelHeros){
+      opponentUniverse = dcHeros;
+    }else{
+      opponentUniverse = marvelHeros;
+    }
+
+    this.setState({
+      choosingOpponent: !this.state.choosingOpponent,
+      universe: opponentUniverse
+    })
+  }
+
+
+  handleRadioClick = (e) => {
     console.log(e.target.value);
     if (e.target.value === "DC") {
       this.setState({
@@ -40,15 +90,28 @@ class App extends React.Component {
 
   componentDidUpdate(prevProps,prevState){
     console.log("names from did update",prevState.heroName, this.state.heroName)
-    if(prevState.heroName !== this.state.heroName){
-      axios.get(`http://localhost:8080/${this.state.heroName}`)
-      .then(res => {
-        console.log("res from id update",res.data);
-        this.setState({
-          heroData: res.data,
+    if(this.state.choosingOpponent === false){
+      if(prevState.heroName !== this.state.heroName){
+        axios.get(`http://localhost:8080/${this.state.heroName}`)
+        .then(res => {
+          console.log("res from id update",res.data);
+          this.setState({
+            heroData: res.data,
+          })
         })
-      })
+      }
+    }else if(this.state.choosingOpponent === true) {
+      if(prevState.opponentName !== this.state.opponentName){
+        axios.get(`http://localhost:8080/${this.state.opponentName}`)
+        .then(res => {
+          console.log("res from id update",res.data);
+          this.setState({
+            opponentData: res.data,
+          })
+        })
+      }
     }
+
   }
 
   render() {
@@ -57,7 +120,7 @@ class App extends React.Component {
       <Router className='App'>
         <Switch>
           <Route path='/' exact render={(routerProps) => {
-            return <Home appState={this.state} handleClick={this.handleClick} handleChange={this.handleChange} {...routerProps} />
+            return <Home appState={this.state} handleRadioClick={this.handleRadioClick} handleClick={this.handleClick} handleChange={this.handleChange} {...routerProps} />
           }} />
           <Route path='/opponent' render={(routerProps) => {
             return <Opponent appState={this.state} {...routerProps} />
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,32 +3,63 @@ import Dropdown from '../../components/Dropdown/Dropdown.js'
 
 
 function Home(props) {
-    let {appState, handleClick, handleChange, showHero} = props;
-    console.log("appstate", appState);
+    let { appState, handleRadioClick, handleChange, handleClick } = props;
+    let heroData = appState.heroData;
+    let opponentData = appState.opponentData;
+    console.log("appstate", appState, heroData);
 
     return (
         <div className="selection" >
             <div className='selection__left'>
-
+                <div className='selection__card'>
+                    <img className='selection__card-image' alt='charecter' src={heroData.image} />
+                    <h3>{heroData.name}</h3>
+                    <ul className='selection__card-list'>
+                        <li className='selection__card-item'>intelligence: {heroData.powerstats.intelligence}</li>
+                        <li className='selection__card-item'>strength: {heroData.powerstats.strength}</li>
+                        <li className='selection__card-item'>speed: {heroData.powerstats.speed}</li>
+                        <li className='selection__card-item'>durability: {heroData.powerstats.durability}</li>
+                        <li className='selection__card-item'>power: {heroData.powerstats.power}</li>
+                        <li className='selection__card-item'>combat: {heroData.powerstats.combat}</li>
+                    </ul>
+                </div>
             </div>
             <div className='selection__middle'>
-            <h1 className='selection__title'>Choose hero</h1>
-            <form>
-                <label>
-                    <input onClick={handleClick} type="radio" name="universe" value="marvel" defaultChecked className='selection__radio-button' />
-                    Marvel
-                </label>
-                <label>
-                    <input onClick={handleClick} type="radio" name="universe" value="DC" className='selection__radio-button' />
-                    DC
-                </label>
-            </form>
-                <Dropdown list={appState.universe}  hero={appState.heroName} handleChange={handleChange} />
-                <button onClick={props.showHero} className='selection__button action-button'>Next</button>
-            </div>
-            <div className='selection__right'>
+                <h1 className='selection__title'>Choose {appState.choosingOpponent ? 'Opponent' : 'Hero'}</h1>
+                {!appState.choosingOpponent &&
+                    <form>
+                        <label>
+                            <input onClick={handleRadioClick} type="radio" name="universe" value="marvel" defaultChecked className='selection__radio-button' />
+                            Marvel
+                        </label>
+                        <label>
+                            <input onClick={handleRadioClick} type="radio" name="universe" value="DC" className='selection__radio-button' />
+                            DC
+                        </label>
+                    </form>
+                }
+                {!appState.choosingOpponent && <Dropdown list={appState.universe} hero={appState.heroName} handleChange={handleChange} />}
+                {appState.choosingOpponent && <Dropdown list={appState.universe} opponent={appState.OpponentName} handleChange={handleChange} />}
 
+                <button onClick={handleClick} className='selection__button action-button'>Next</button>
             </div>
+            {appState.choosingOpponent &&
+                <div className='selection__right'>
+                    <div className='selection__card'>
+                        <img className='selection__card-image' alt='charecter' src={opponentData.image} />
+                        <h3>{opponentData.name}</h3>
+                        <ul className='selection__card-list'>
+                            <li className='selection__card-item'>intelligence: {opponentData.powerstats.intelligence}</li>
+                            <li className='selection__card-item'>strength: {opponentData.powerstats.strength}</li>
+                            <li className='selection__card-item'>speed: {opponentData.powerstats.speed}</li>
+                            <li className='selection__card-item'>durability: {opponentData.powerstats.durability}</li>
+                            <li className='selection__card-item'>power: {opponentData.powerstats.power}</li>
+                            <li className='selection__card-item'>combat: {opponentData.powerstats.combat}</li>
+                        </ul>
+                    </div>
+                </div>
+            }
+
         </div>
     )
 }
