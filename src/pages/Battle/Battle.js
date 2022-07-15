679caa5d350e3c8f5388161287e8f4f5f91979f9
Jordan Hoffman <jghoffman01@gmail.com>
2022-07-15 09:54:24 -0400

    battle progress

--- src/pages/Battle/Battle.js
+++ src/pages/Battle/Battle.js
@@ -1,10 +1,36 @@
 
 
 
-function Battle(props){
-    return(
-        <p>hi</p>
-    )
+function Battle(props) {
+  //replace with props
+  const hero = {
+    "name": "Captain Planet",
+    "img": "https://www.superherodb.com/pictures2/portraits/10/100/1285.jpg",
+
+    "intelligence": "50",
+    "strength": "88",
+    "speed": "75",
+    "durability": "80",
+    "power": "100",
+    "combat": "60"
+  }
+
+  const enemy = {
+    "name": "Magneto",
+    "img": "https://www.superherodb.com/pictures2/portraits/10/100/12.jpg",
+    "intelligence": "88",
+    "strength": "80",
+    "speed": "27",
+    "durability": "84",
+    "power": "91",
+    "combat": "80"
+  }
+
+  return (
+    <div className="battlepage">
+
+    </div>
+  )
 }
 
 
